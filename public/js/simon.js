80b1b7586a2de5ecfc0ebb2de8c6c8bb89dbe206
xconstaud <xconstaud@gmail.com>
2017-03-05 01:30:43 -0600

    Add new sounds, remove unused sounds, update layout, add basic logic

--- public/js/simon.js
+++ public/js/simon.js
@@ -1,25 +1,30 @@
-var audio = new Audio('/sounds/engine.mp3');
-// audio.volume = 0.4;
+//    ===========Sound Variables==============
+var voyagerBridge = new Audio("/sounds/voyagerBridge.mp3");
+var redAlertSound = new Audio("/sounds/redAlert4.mp3");
+var computerActivate = new Audio("/sounds/computerActivate.mp3");
+var computerWork = new Audio("/sounds/computerWork.mp3");
+var computerScroll = new Audio("/sounds/computerScroll.mp3");
+var computerPowerup = new Audio("/sounds/computerPowerup.mp3");
+var computerPowerup2 = new Audio("/sounds/computerPowerup2.mp3");
+var computerDamage = new Audio("/sounds/computerDamage.mp3");
+//Sound Volume
+computerDamage.volume = 0.1;
 
 
-
-
-
-//HTML Variables
+// ============HTML Variables=================
 //DATA DIV
 var shieldTitle = $("#shieldTitle");
 var topLeftBox = $("#topLeftBox"); 
-var topLeftHook = $("#topLeftHook"); 
 var leftHookMiniBox = $("#leftHookMiniBox");
 var topShortBox = $("#topShortBox");
 var topLongBox = $("#topLongBox");
 var topFinalBox = $("#topFinalBox");
+
 //GAME DIV
 var botFinalBox =$("#botFinalBox");
 var botLongBox = $("#botLongBox");
 var thinBox = $("#thinBox"); 
 var midHookMiniBox = $("#midHookMiniBox"); 
-var midHook = $("#midHook"); 
 var midLeftBox = $("#midLeftBox"); 
 var midLeftBoxSm = $("#midLeftBoxSm"); 
 var botLeftBox = $("#botLeftBox"); 
@@ -33,98 +38,395 @@ var fifthLine = $("#fifthLine");
 var sixthLine = $("#sixthLine"); 
 var seventhLine = $("#seventhLine"); 
 
+//Voyager and shields
+var voyager = $("#voyager"); 
+var shields = $("#voyagerShields"); 
+var indicatorTop = $("#indicatorTop");
+var indicatorBottom = $("#indicatorBottom");
+var indicatorRight = $("#indicatorRight");
+var indicatorLeft = $("#indicatorLeft");
+
+//readyPrompt
+var readyPrompt = $("#promptReady");
+var readyYes = $("#readyYes");
+var purple = $(".purple");
+var mustard = $(".mustard");
+var pink = $(".pink");
+var brown = $(".brown");
+
+//hooks
+var topLeftHook = $("#topLeftHook"); 
+var topLeftHookRed = $("#topLeftHookRed"); 
+var topLeftHookWhite = $("#topLeftHookWhite"); 
+var midHook = $("#midHook"); 
+var midHookRed = $("#midHookRed"); 
+var midHookWhite = $("#midHookWhite"); 
+var midHookClass = $(".midHook");
+var topLeftHookClass = $(".topLeftHook");
+
+
+readyYes.click(function(event) {
+    readyPrompt.css({
+        display: 'none'});
+    redAlert();
+    redAlertSoundRepeat();
+    setTimeout(function() { 
+        game();
+    }, 5000);
+});
 
 
 
 
-//Startup
-//first
-topLeftBox.animate({
-    opacity: '1'
-    
-},1000);
-botLeftBox.animate({
-    opacity: '1'
-    
-},1000);
-//second
-topLeftHook.delay(300).animate({
-    opacity: '1'
-},1000);
-
-midLeftBoxSm.delay(300).animate({
-    opacity: '1'
-},1000);
-//third
-midLeftBox.delay(800).animate({
-    opacity: '1'
-},1000);
-midHook.delay(800).animate({
-    opacity: '1'
-},1000);
-//fourth
-leftHookMiniBox.delay(900).animate({
-    opacity: '1'
-},1000);
-midHookMiniBox.delay(900).animate({
-    opacity: '1'
-},1000);
-//fifth
-topShortBox.delay(1000).animate({
-    opacity: '1'
-},1000);
-thinBox.delay(1000).animate({
-    opacity: '1'
-},1000);
-//sixth
-topShortBox.delay(1200).animate({
-    opacity: '1'
-},1000);
-thinBox.delay(1200).animate({
-    opacity: '1'
-},1000);
-//seventh
-topLongBox.delay(1400).animate({
-    opacity: '1'
-},1000);
-botLongBox.delay(1400).animate({
-    opacity: '1'
-},1000);
-//eighth
-topFinalBox.delay(1600).animate({
-    opacity: '1'
-},1000);
-botFinalBox.delay(1600).animate({
-    opacity: '1'
-},1000);
 
 
-//title
-shieldTitle.delay(2000).animate({
-    opacity: '1'
-},2000);
 
-//Data Output Random fade in
-firstLine.delay(4200).animate({
-    opacity: '1'
-},200);
-secondLine.delay(4300).animate({
-    opacity: '1'
-},200);
-thirdLine.delay(4400).animate({
-    opacity: '1'
-},200);
-fourthLine.delay(4500).animate({
-    opacity: '1'
-},200);
-fifthLine.delay(4600).animate({
-    opacity: '1'
-},200);
-sixthLine.delay(4700).animate({
-    opacity: '1'
-},200);
-seventhLine.delay(4800).animate({
-    opacity: '1'
-},200);
 
-audio.delay(4800).play();
+
+// =======================GAME LOGIC========================
+var quadrant = 0;
+
+function game() {
+    var userCorrect = true;
+    var quadrantOutputArray = [];
+    var userInputArray = [];
+    while (userCorrect == true) {
+        quadrant = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+        quadrantOutputArray.push(quadrant);
+        computerDamage.play();
+        console.log(quadrantOutputArray);
+        for (i = 0; i < quadrantOutputArray.length; i++) {
+            if (quadrantOutputArray[i] == 1) {
+                indicatorTopDisplay();
+            }else if (quadrantOutputArray[i] == 2) {
+                indicatorRightDisplay();
+            }else if (quadrantOutputArray[i] == 3) {
+                indicatorBottomDisplay();
+            }else if (quadrantOutputArray[i] == 4) {
+                indicatorLeftDisplay();
+            }
+
+            if (i == quadrantOutputArray.length - 1) {
+                var checkUser = setTimeout(function () {
+                    //failed
+                }, 3000);
+            }
+        }
+        
+        
+        userCorrect = false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+// ==========================REACTIVE ANIMATIONS========================
+var isFirst = false;
+function redAlert() {
+    //Set All Red
+    purple.css({background: 'red'});
+    mustard.css({background: 'red'});
+    pink.css({background: 'red'});
+    brown.css({background: 'red'});
+    topLeftHookClass.css({
+        opacity: '1'
+    });
+    midHookClass.css({
+        opacity: '1'
+    });
+    //Done like this to load all hook images at page load to prevent style clipping as color changes first sequence
+    midHook.hide();
+    midHookRed.show();
+    topLeftHook.hide();
+    topLeftHookRed.show();
+
+    //White Snake effect
+    setTimeout(function() { 
+        botLeftBox.css({background: 'white'});
+    }, 200);
+    setTimeout(function() { 
+        botLeftBox.css({background: 'red'});
+    }, 400);
+    setTimeout(function() { 
+        midLeftBoxSm.css({background: 'white'});
+    }, 400);
+    setTimeout(function() { 
+        midLeftBoxSm.css({background: 'red'});
+    }, 600);
+    setTimeout(function() { 
+        midLeftBox.css({background: 'white'});
+    }, 600);
+    setTimeout(function() { 
+        midLeftBox.css({background: 'red'});
+    }, 800);
+    setTimeout(function() { 
+        midHookRed.hide();
+        midHookWhite.show();
+    }, 800);
+    setTimeout(function() { 
+        midHookWhite.hide();
+        midHookRed.show();
+    }, 1000);
+    setTimeout(function() { 
+        midHookMiniBox.css({background: 'white'});
+    }, 1000);
+    setTimeout(function() { 
+        midHookMiniBox.css({background: 'red'});
+    }, 1200);
+    setTimeout(function() { 
+        thinBox.css({background: 'white'});
+    }, 1200);
+    setTimeout(function() { 
+        thinBox.css({background: 'red'});
+    }, 1400);
+    setTimeout(function() { 
+        botLongBox.css({background: 'white'});
+    }, 1400);
+    setTimeout(function() { 
+        botLongBox.css({background: 'red'});
+    }, 1600);
+    setTimeout(function() { 
+        botFinalBox.css({background: 'white'});
+    }, 1600);
+    setTimeout(function() { 
+        botFinalBox.css({background: 'red'});
+    }, 1800);
+
+    //TOP
+    setTimeout(function() { 
+        topLeftBox.css({background: 'white'});
+    }, 200);
+    setTimeout(function() { 
+        topLeftBox.css({background: 'red'});
+    }, 400);
+    setTimeout(function() { 
+        topLeftHookRed.hide();
+        topLeftHookWhite.show();
+    }, 400);
+     setTimeout(function() { 
+        topLeftHookWhite.hide();
+        topLeftHookRed.show();
+    }, 600);
+    setTimeout(function() { 
+        leftHookMiniBox.css({background: 'white'});
+    }, 600);
+    setTimeout(function() { 
+        leftHookMiniBox.css({background: 'red'});
+    }, 800);
+    setTimeout(function() { 
+        topShortBox.css({background: 'white'});
+    }, 800);
+    setTimeout(function() { 
+        topShortBox.css({background: 'red'});
+    }, 1000);
+    setTimeout(function() { 
+        topLongBox.css({background: 'white'});
+    }, 1000);
+    setTimeout(function() { 
+        topLongBox.css({background: 'red'});
+    }, 1200);
+    setTimeout(function() { 
+        topFinalBox.css({background: 'white'});
+    }, 1200);
+    setTimeout(function() { 
+        topFinalBox.css({background: 'red'}); 
+    }, 1400);
+    setTimeout(redAlert, 2000);
+}
+
+
+function indicatorTopDisplay() {
+    console.log("top");
+    indicatorTop.animate({
+        opacity: '1'
+    },1000);
+    indicatorTop.delay(1000).animate({
+        opacity: '0'
+    },1000);
+}
+
+
+function indicatorRightDisplay() {
+    console.log("right");
+    indicatorRight.animate({
+        opacity: '1'
+    },1000);
+    indicatorRight.delay(1000).animate({
+        opacity: '0'
+    },1000);
+}
+
+function indicatorBottomDisplay() {
+    console.log("bottom");
+    indicatorBottom.animate({
+        opacity: '1'
+    },1000);
+    indicatorBottom.delay(1000).animate({
+        opacity: '0'
+    },1000);
+}
+
+function indicatorLeftDisplay() {
+    console.log("left");
+    indicatorLeft.animate({
+        opacity: '1'
+    },1000);
+    indicatorLeft.delay(1000).animate({
+        opacity: '0'
+    },1000);
+}
+
+
+
+
+//===================General Animations===================
+
+// ====================Start Up=================
+function startup() {
+    //Play Computer Startup Sound
+    startupSounds();
+
+    //--Animation Sequence--
+    //first
+    topLeftBox.animate({
+        opacity: '1' 
+    },1000);
+    botLeftBox.animate({
+        opacity: '1'
+        
+    },1000);
+    //second
+    topLeftHook.delay(300).animate({
+        opacity: '1'
+    },1000);
+
+    midLeftBoxSm.delay(300).animate({
+        opacity: '1'
+    },1000);
+    //third
+    midLeftBox.delay(800).animate({
+        opacity: '1'
+    },1000);
+    midHook.delay(800).animate({
+        opacity: '1'
+    },1000);
+    //fourth
+    leftHookMiniBox.delay(900).animate({
+        opacity: '1'
+    },1000);
+    midHookMiniBox.delay(900).animate({
+        opacity: '1'
+    },1000);
+    //fifth
+    topShortBox.delay(1000).animate({
+        opacity: '1'
+    },1000);
+    thinBox.delay(1000).animate({
+        opacity: '1'
+    },1000);
+    //sixth
+    topShortBox.delay(1200).animate({
+        opacity: '1'
+    },1000);
+    thinBox.delay(1200).animate({
+        opacity: '1'
+    },1000);
+    //seventh
+    topLongBox.delay(1400).animate({
+        opacity: '1'
+    },1000);
+    botLongBox.delay(1400).animate({
+        opacity: '1'
+    },1000);
+    //eighth
+    topFinalBox.delay(1600).animate({
+        opacity: '1'
+    },1000);
+    botFinalBox.delay(1600).animate({
+        opacity: '1'
+    },1000);
+
+    //title
+    shieldTitle.delay(2000).animate({
+        opacity: '1'
+    },2000);
+    setTimeout(function() { 
+        computerWork.play();
+    }, 2000);
+
+    //Data Output Random fade in
+    setTimeout(function() { 
+        computerScroll.play();
+    }, 4200);
+    firstLine.delay(4200).animate({
+        opacity: '1'
+    },200);
+    secondLine.delay(4300).animate({
+        opacity: '1'
+    },200);
+    thirdLine.delay(4400).animate({
+        opacity: '1'
+    },200);
+    fourthLine.delay(4500).animate({
+        opacity: '1'
+    },200);
+    fifthLine.delay(4600).animate({
+        opacity: '1'
+    },200);
+    sixthLine.delay(4700).animate({
+        opacity: '1'
+    },200);
+    seventhLine.delay(4800).animate({
+        opacity: '1'
+    },200);
+
+    //Voyager and shields
+    setTimeout(function() { 
+        computerPowerup2.play();
+    }, 4900);
+    voyager.delay(4900).animate({
+        opacity: '1'
+    },3000);
+    setTimeout(function() { 
+        computerPowerup.play();
+    }, 6000);
+    shields.delay(6000).animate({
+        opacity: '1'
+    },2000);
+
+    //END STARTUP
+    //Prompt Ready, launch effects and game on ready
+    readyPrompt.delay(8000).animate({
+        opacity: '1'
+    },1000);
+}
+
+
+
+
+//==================Sounds====================
+
+function redAlertSoundRepeat() {
+    var x = 10;
+    redAlertSound.play();
+    setTimeout(redAlertSoundRepeat, 10000);
+}
+
+function startupSounds() {
+    voyagerBridge.play();
+    voyagerBridge.volume = 0.3;
+    computerActivate.play();
+}
+
+
+//============On page Load=============
+//run startup function to load elements
+startup();
